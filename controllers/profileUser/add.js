d290e88f6d9a3f39d0a718aaa62e3b6f48c14499
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-06-16 21:40:42 +0700

    fixing the base put and post code in both file, add and update .js

--- controllers/profileUser/add.js
+++ controllers/profileUser/add.js
@@ -20,6 +20,7 @@ module.exports = async (req, res) => {
             })
         }
     } catch (err) {
-
+        console.log(err);
+        throw new Error(err);
     }
 }
--- controllers/profileUser/update.js
+++ controllers/profileUser/update.js
@@ -1,17 +1,27 @@
-const { profile, user } = require('../../models/profile');
+const { profile } = require('../../models/profile');
 
 module.exports = async (req, res) => {
     try {
         // Update User
-        const updateUser = await user.update({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
+        const updateProfile = await profile.update({
+            photo: req.body.photo,
+            phone: req.body.phone,
+            address: req.body.address,
+            city: req.body.city
         }, {
             where: {
                 id: req.params.id
             }
-        });
+        })
+        if (!updateProfile) {
+            res.status(404).json({
+                message: 'Failed to update data'
+            })
+        } else {
+            res.status(200).json({
+                message: 'Successfully updated data'
+            })
+        }
     } catch (err) {
         console.log(err);
         throw new Error(err);
