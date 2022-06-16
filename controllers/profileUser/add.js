b679a36bb0d6eb5dc083ed37e59002514ee88a2f
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-06-16 21:26:45 +0700

    fixing the post material for add.js

--- controllers/profileUser/add.js
+++ controllers/profileUser/add.js
@@ -1,5 +1,25 @@
 const { profile } = require('../../models/profile');
 
 module.exports = async (req, res) => {
+    // add the profile user
+    try {
+        const addProfile = await profile.create({
+            photo: req.body.photo,
+            phone: req.body.phone,
+            address: req.body.address,
+            city: req.body.city
+        })
+        if (!addProfile) {
+            res.status(404).json({
+                status: 'fail',
+                message: 'Failed to add data'
+            })
+        } else {
+            res.status(200).json({
+                data: addProfile
+            })
+        }
+    } catch (err) {
 
+    }
 }
