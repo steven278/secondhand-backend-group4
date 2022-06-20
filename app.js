a24c6bee9d401909c9b7b7c718c09c852871d8e5
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-06-20 21:09:05 +0700

    fixing the app js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = express();
 
 const baseUrl = process.env.BASE_URL || '/api/v1.0';
 const { router } = require('./routes/routes');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.use(`${baseUrl}`, router);
 
