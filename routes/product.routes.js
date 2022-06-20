f1f7e68cab1e475a3c947da24c422e545c6344f3
steven278 <darkavenger347@gmail.com>
2022-06-20 19:31:34 +0700

    fixed product update feature

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,11 +5,11 @@ const getAll = require('../controllers/products/getAll');
 const getById = require('../controllers/products/getById');
 const create = require('../controllers/products/create');
 const update = require('../controllers/products/update');
-const { productValidation, validate } = require('../validation/product.validator');
+const { productValidation, productUpdateValidation, validate } = require('../validation/product.validator');
 
 router.get('/', getAll);
 router.get('/:id', getById);
 router.post('/', upload.array('photos', 4), productValidation(), validate, uploadWithCloudinary, create);
-router.put('/', update);
+router.put('/:id', upload.array('photos', 4), productUpdateValidation(), validate, uploadWithCloudinary, update);
 
 module.exports = router;
--- validation/product.validator.js
+++ validation/product.validator.js
@@ -10,6 +10,15 @@ const productValidation = () => {
     ]
 }
 
+const productUpdateValidation = () => {
+    return [
+        check('name').isLength({ min: 2 }).withMessage('must be at least 2 chars long'),
+        check('price').isNumeric().withMessage('must be a number'),
+        check('category_id').isNumeric().withMessage('must be a number'),
+        check('description').isLength({ min: 10 }).withMessage('must be at least 10 chars long'),
+    ]
+}
+
 const validate = (req, res, next) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
@@ -22,5 +31,6 @@ const validate = (req, res, next) => {
 
 module.exports = {
     productValidation,
+    productUpdateValidation,
     validate
 };
