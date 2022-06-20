7c3d0860954932a1a392f7518465a66f92b8e995
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-06-20 21:54:41 +0700

    change morgan into dev onnly

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const baseUrl = process.env.BASE_URL || '/api/v1.0';
+const baseUrl = process.env.BASE_URL || '/secondhand';
 const { router } = require('./routes/routes');
 const morgan = require('morgan');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(morgan('dev'));
 
 app.use(`${baseUrl}`, router);
 
