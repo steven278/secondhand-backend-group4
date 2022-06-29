8cd78910733f9be79ce7a49e8e298cd463a4150d
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-06-30 00:03:01 +0700

    clearing code on regist and fix some bugs, but doesnt clear

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,34 +4,6 @@ const jwt = require('jsonwebtoken');
 const transportEmail = require('../helper/mailer');
 require('dotenv').config();
 
-const readUserDataById = async (req, res, next) => {
-    // get the user by id
-    try {
-        const userData = await User.findOne({
-            where: {
-                id: req.params.id
-            },
-            attributes: {
-                exclude: ['createdAt', 'updatedAt', 'password']
-            },
-        })
-        if (!userData) {
-            res.status(404).json({
-                status: 'fail',
-                message: 'There is no user with this id'
-            })
-        } else {
-            res.status(200).json({
-                status: 'success',
-                data: userData
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
-}
-
 const loginUser = (req, res, next) => {
     // login the user
 }
@@ -42,51 +14,49 @@ const registUser = async (req, res, next) => {
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
-            profile_id: id,
-            isVerified: false
+            isVerified: false,
         })
+        console.log(register)
         if (register) {
-            await profile.create({
-                profile_id: register.id,
-                photo: NULL,
-                phone: NULL,
-                address: NULL,
-                city: NULL,
+            await Profile.create({
+                photo: null,
+                phone: null,
+                address: null,
+                city: null,
             })
         };
 
-        const clearRegist = await user.findOne({
+        const clearRegist = await User.findOne({
             where: {
                 id: register.id
             },
             include: [{
-                model: profile
+                model: Profile
             }]
         })
 
-        console.log(register)
-        const { email } = req.body;
-        const greet = `Thank you for registering your account in our website`;
-        const emailResponse = await transportEmail(email, greet);
-        if (!register) {
-            res.status(404).json({
-                status: 'fail',
-                message: 'cant regist'
-            })
-        } else {
-            res.status(201).json({
-                status: "success",
-                data: register,
-                message: `Email sent to ${emailResponse.accepted.join(',').split(',')}`
-            });
-        }
+        // console.log(register)
+        // const { email } = req.body;
+        // const greet = `Thank you for registering your account in our website`;
+        // const emailResponse = await transportEmail(email, greet);
+        // if (!register) {
+        //     res.status(404).json({
+        //         status: 'fail',
+        //         message: 'cant regist'
+        //     })
+        // } else {
+        //     res.status(201).json({
+        //         status: "success",
+        //         data: register,
+        //         message: `Email sent to ${emailResponse.accepted.join(',').split(',')}`
+        //     });
+        // }
 
     } catch (err) {
         next(err);
     }
 }
 module.exports = {
-    readUserDataById,
     loginUser,
     registUser
 }
