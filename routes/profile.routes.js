195a9cd84caccfe9101642497a1cd8fe39303fef
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-06-29 21:28:38 +0700

    delete the endpoint add profile useless

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addProfile, readProfile, updateProfile } = require('../controllers/profile.controller');
+const { readProfile, updateProfile } = require('../controllers/profile.controller');
 const upload = require('../helper/multer');
 
 router.get('/:id', readProfile);
-router.post('/', upload.single('photo'), addProfile)
-router.put('/:id', upload.single('photo'), updateProfile)
+router.put('/:id', upload.single('photo'), updateProfile);
 
 module.exports = router;
