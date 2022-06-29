ea910e5610b318c638226e21f97d4556aa8dae50
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-06-29 19:30:18 +0700

    adding the multer

---
+++ helper/multer.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+    filename: (req, file, cb) => {
+        const fileType = file.mimetype.split('/')[1];
+        cb(null, file.fieldname + '-' + Date.now() + `${fileType}`);
+    }
+});
+
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        // The function should call cb with a boolean
+        // to indicate if the file should be accepted
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') return cb(null, true);
+        // To reject this file pass false, like so:
+        cb(null, false);
+        // You can always pass an error if something goes wrong:
+        return cb(new Error('Wrong filetype'));
+    },
+    limits: {
+        fileSize: 10000000
+    }
+});
+
+module.exports = upload;
