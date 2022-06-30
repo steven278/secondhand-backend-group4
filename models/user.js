18be47d2c75dbc517542e29198b10e657a5826ec
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-06-30 19:51:33 +0700

    fixing association on profile models and user models

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasOne(models.Profile, {
+      User.belongsTo(models.Profile, {
         foreignKey: 'profile_id'
       })
       User.belongsTo(models.Role, {
