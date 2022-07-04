fd806a85988a04711932f14eb0f4deedc786ce0b
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-07-04 23:13:09 +0700

    file changes in create profile to be first to called

---
+++ seeders/20220609143452-create-profile-data.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const profiles = require('../masterdata/profiles.json').map((profiles) => {
+  profiles.createdAt = new Date();
+  profiles.updatedAt = new Date();
+  return profiles;
+})
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Profiles', profiles, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Profiles', null, { truncate: true, restartIdentity: true });
+  }
+};
