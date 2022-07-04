5d514ac8672622d37e27fe04ddb1376e652d49f7
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-07-04 22:06:02 +0700

    add the post method on profile for cloudinary too

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { readProfile, updateProfile } = require('../controllers/profile.controller');
+const validation = require('../helper/validation')
+const { readProfile, addProfile, updateProfile } = require('../controllers/profile.controller');
 const upload = require('../helper/multer');
+const cloudinary = require('../helper/uploadService');
 
 router.get('/:id', readProfile);
-router.put('/:id', upload.single('photo'), updateProfile);
+router.post('/', validation, upload.single('photo'), cloudinary, addProfile);
+router.put('/:id', validation, upload.single('photo'), updateProfile);
 
 module.exports = router;
