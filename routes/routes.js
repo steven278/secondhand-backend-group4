c30fba0f6fb4fae9da3ccc74602bf1a214c7febe
steven278 <darkavenger347@gmail.com>
2022-07-04 16:49:09 +0700

    improve res status in error handling

--- routes/routes.js
+++ routes/routes.js
@@ -14,19 +14,19 @@ router.use('/transactions', transactions);
 
 router.use((err, req, res, next) => {
     if (err.name === 'SequelizeDatabaseError' || err.name === 'SequelizeUniqueConstraintError' || err.name === 'ReferenceError' || err.name === 'SequelizeForeignKeyConstraintError') {
-        res.status(400).json({
+        return res.status(400).json({
             status: 'Bad Request',
             errorName: err.name,
             message: err.message
         });
     } else if (err.name === 'Error' || err.name === 'TypeError') {
-        res.status(404).json({
+        return res.status(404).json({
             status: 'Not Found',
             errorName: err.name,
             message: err.message
         });
     }
-    res.status(500).json({
+    return res.status(500).json({
         status: 'Internal Server Error',
         errorName: err.name,
         message: err.message
