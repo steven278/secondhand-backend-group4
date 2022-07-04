b6fbcaa82b6ef606dc2e229a948688f7dd313f2c
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-07-04 21:32:23 +0700

    adding the upload service in helper

---
+++ helper/uploadService.js
@@ -0,0 +1,30 @@
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+const fs = require('fs');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+
+
+const uploadPhoto = async (req, res, next) => {
+    try {
+        const folderPath = `my_data/photo/${req.file.mimetype.split('/')[1]}`;
+        console.log(req.file.path);
+        const uploadPhoto = await cloudinary.uploader.upload(req.file.path, {
+            folder: folderPath,
+            resource_type: 'photo'
+        });
+        fs.unlinkSync(req.file.path);
+        req.body.video = uploadPhoto.secure_url;
+        next();
+    } catch (err) {
+        fs.unlinkSync(req.file.path);
+        console.log(err)
+    }
+}
+
+module.exports = uploadPhoto;
