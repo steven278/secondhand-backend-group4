bb122ca9cb880ace5ab040ef29a7431d8f5ebccd
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2022-07-05 21:23:59 +0700

    delete add profile routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const validation = require('../helper/validation')
-const { readProfile, addProfile, updateProfile } = require('../controllers/profile.controller');
+const { readProfile, updateProfile } = require('../controllers/profile.controller');
 const upload = require('../helper/multer');
 const cloudinary = require('../helper/uploadService');
 
-router.get('/:id', readProfile);
-router.post('/', validation, upload.single('photo'), cloudinary, addProfile);
-router.put('/:id', validation, upload.single('photo'), updateProfile);
+router.get('/:id', validation, readProfile);
+router.put('/:id', validation, upload.single('photo'), cloudinary, updateProfile);
 
 module.exports = router;
