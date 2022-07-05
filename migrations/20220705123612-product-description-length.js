6122b0832472b73d31efff401069e514cb48e337
steven278 <darkavenger347@gmail.com>
2022-07-05 19:52:58 +0700

    change string length in description column (Products)

---
+++ migrations/20220705123612-product-description-length.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Products', 'description', {
+      allowNull: false,
+      type: Sequelize.STRING(1500)
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Products', 'description')
+  }
+};
