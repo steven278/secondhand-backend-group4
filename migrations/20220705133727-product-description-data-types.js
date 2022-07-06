67c8637adc2e7569647c0486a394da6b111fab7c
steven278 <darkavenger347@gmail.com>
2022-07-06 19:29:38 +0700

    refactor product description data types

---
+++ migrations/20220705133727-product-description-data-types.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Products', 'description', {
+      allowNull: false,
+      type: Sequelize.TEXT
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Products', 'description')
+  }
+};
+
