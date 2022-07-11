2baffadd5055fde07af6ce0d1a310d2810e0fa87
steven278 <darkavenger347@gmail.com>
2022-07-11 19:57:53 +0700

    cors issue fixing

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 
-// const corsOptions = {
-//     origin: '*',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-// // app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(morgan('dev'));
 
-app.use(cors());
+// app.use(cors());
 
 app.use(`${baseUrl}`, router);
 
