9644a486bb50cbdd84c271be9f6c377ee93f48ac
steven278 <darkavenger347@gmail.com>
2022-07-12 22:06:54 +0700

    fixing cors issue

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 
-const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// const corsOptions = {
+//     origin: '*',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
-app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: ['http://localhost:3000', 'http://localhost:5000', ' https://secondhand-group4.herokuapp.com/secondhand', 'https://second-hand-by-group-4.netlify.app'] }));
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(morgan('dev'));
 
