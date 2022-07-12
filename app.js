007145a6a235b49f46fb288df828e84253d1fbac
steven278 <darkavenger347@gmail.com>
2022-07-12 20:33:24 +0700

    fixing cors issue

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    preflightContinue: true,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// const cors = require('cors');
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+//     preflightContinue: true,
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+//Cors Configuration - Start
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next()
+})
+//Cors Configuration - End
 
 const baseUrl = process.env.BASE_URL || '/secondhand';
 const { router } = require('./routes/routes');
