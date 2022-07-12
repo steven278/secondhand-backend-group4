62d2114b92785aade701bfe30cb5fee4c74337cd
steven278 <darkavenger347@gmail.com>
2022-07-12 19:33:12 +0700

    fixing cors issue for login

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 const baseUrl = process.env.BASE_URL || '/secondhand';
 const { router } = require('./routes/routes');
@@ -11,14 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(morgan('dev'));
 
