1722bab7e2fd357381f6e0cb0f9c843ea6a84185
steven278 <darkavenger347@gmail.com>
2022-07-12 20:26:32 +0700

    fixing cors issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const corsOptions = {
-    origin: false,
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     preflightContinue: true,
