ac97c8b833514c0a4e2677334af76c0f1b391fa1
steven278 <darkavenger347@gmail.com>
2022-07-12 20:09:07 +0700

    fixing cors issue

--- app.js
+++ app.js
@@ -10,25 +10,25 @@ const morgan = require('morgan');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization', 'http://localhost:3000');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-});
+// app.use(cors());
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization', 'http://localhost:3000');
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 
-// const corsOptions = {
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(morgan('dev'));
 
