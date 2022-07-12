5520b6ede10ccf99953c3cb9e927584253e446c6
steven278 <darkavenger347@gmail.com>
2022-07-12 20:38:19 +0700

    fixing cors issue

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const cors = require('cors');
-// const corsOptions = {
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-//     preflightContinue: true,
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-
-// app.use(cors(corsOptions));
-//Cors Configuration - Start
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "POST, PUT, PATCH, GET, DELETE"
-        )
-        return res.status(200).json({})
-    }
-    next()
-})
-//Cors Configuration - End
+const cors = require('cors');
 
 const baseUrl = process.env.BASE_URL || '/secondhand';
 const { router } = require('./routes/routes');
@@ -37,18 +11,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(morgan('dev'));
+
+// app.use(cors());
 
 app.use(`${baseUrl}`, router);
 
 module.exports = app;
-
-// app.use(cors());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization', 'http://localhost:3000');
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
-//         return res.status(200).json({});
-//     }
-//     next();
-// });
