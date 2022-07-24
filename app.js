9e721f8d4b26a09729a8bf719c3543f07349afa2
steven278 <darkavenger347@gmail.com>
2022-07-24 23:32:22 +0700

    remove some unwanted comments

--- app.js
+++ app.js
@@ -10,21 +10,6 @@ const morgan = require('morgan');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
-
-// const corsOptions = {
-//     origin: '*',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-
 app.use(cors({
     origin: ['http://localhost:3000',
         'http://localhost:5000',
@@ -33,7 +18,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
 }));
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
 app.use(morgan('dev'));
 
 // app.use(cors());
