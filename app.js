4b673ba5a4cbe5e551e343053a09be92bc15ecd0
steven278 <62170575+steven278@users.noreply.github.com>
2024-02-04 14:30:12 +0700

    Update app.js cors

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: ['http://localhost:3000',
         'http://localhost:5000',
-        'https://secondhand-group4.herokuapp.com', 'https://second-hand-by-group-4.netlify.app', 'https://second-hand-by-group-4.vercel.app', 'https://second-hand-by-group-4.herokuapp.com'],
+        'https://secondhand-group4.herokuapp.com', 'https://second-hand-by-group-4.netlify.app', 'https://second-hand-by-group-4.vercel.app', 'https://second-hand-by-group-4.herokuapp.com', 
+            'https://second-hand-front-end-group-4-forked.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
 }));
